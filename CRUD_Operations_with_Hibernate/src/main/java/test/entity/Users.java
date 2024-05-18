adce3ff445147af4e7fc8d5e82a29249eeadf22f
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-05-18 15:49:39 +0530

    CRUD Operations with hibernate

--- CRUD_Operations_with_Hibernate/src/main/java/test/entity/Users.java
+++ CRUD_Operations_with_Hibernate/src/main/java/test/entity/Users.java
@@ -1,5 +1,77 @@
 package test.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="users")
+@Table(name="users")
 public class Users {
+	@Id
+	
+	@Column(name="user_id")
+	int user_id;
+	
+	@Column(name="username")
+	String username;
+	
+	@Column(name="password")
+	String password;
+	
+	@Column(name="first_name")
+	String first_name;
+	
+	@Column(name="last_name")
+	String last_name;
+
+	
+	public Users(String username, String password, String first_name, String last_name) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
 
 }
